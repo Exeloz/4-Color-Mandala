42413710da712c7a9ea21dd72513ce7a62c20d74
exeloz <christophercoulombe@gmail.com>
2026-03-05 13:53:01 -0500

    fix: Bouton undo pas disponible quand mandala fini

--- app/src/screens/coloringScreen.cpp
+++ app/src/screens/coloringScreen.cpp
@@ -182,7 +182,9 @@ void ColoringScreen::draw() {
         drawColorPalette();
     }
     backButton.draw();
-    undoButton.draw();
+    if (!readOnlyMode) {
+        undoButton.draw();
+    }
     if (!readOnlyMode) {
         validateButton.draw();
     }
@@ -336,10 +338,15 @@ void ColoringScreen::layoutTopButtons() {
 
     float undoButtonWidth = mobileLayout ? 126.0f * uiScale : 110.0f;
     float undoButtonHeight = topButtonHeight;
-    float undoButtonX = GetScreenWidth() - undoButtonWidth - rightMargin;
-    float undoButtonY = GetScreenHeight() - undoButtonHeight - (20.0f * uiScale);
-    undoButton.setPosition(undoButtonX, undoButtonY);
-    undoButton.setSize(undoButtonWidth, undoButtonHeight);
+    if (readOnlyMode) {
+        undoButton.setPosition(0.0f, 0.0f);
+        undoButton.setSize(0.0f, 0.0f);
+    } else {
+        float undoButtonX = GetScreenWidth() - undoButtonWidth - rightMargin;
+        float undoButtonY = GetScreenHeight() - undoButtonHeight - (20.0f * uiScale);
+        undoButton.setPosition(undoButtonX, undoButtonY);
+        undoButton.setSize(undoButtonWidth, undoButtonHeight);
+    }
 
     float validateButtonX = GetScreenWidth() - 2*mainButtonWidth - rightMargin;
     if (readOnlyMode) {
